ff4d43d8065dfaccb758ae45e2a2af986e3f6808
teacher <net94.teacher@gmail.com>
2020-04-14 11:42:14 +0900

    리팩터링 초기화

---
+++ src/main/java/refactoring_concept/Rental.java
@@ -0,0 +1,20 @@
+package refactoring_concept;
+
+public class Rental {
+    private Movie movie;
+    private int daysRented;
+    
+    public Rental(Movie aMovie, int aDaysRented) {
+        this.movie = aMovie;
+        this.daysRented = aDaysRented;
+    }
+
+    public Movie getMovie() {
+        return this.movie;
+    }
+
+    public int getDaysRented() {
+        return this.daysRented;
+    }
+
+}
