361556af61189ba0d5bf1ebfa45a73895a69512e
teacher <net94.teacher@gmail.com>
2020-04-16 09:44:14 +0900

    Replace Temp with Query(thisAmount->each.getCharge())

--- src/main/java/refactoring_concept/Customer.java
+++ src/main/java/refactoring_concept/Customer.java
@@ -26,8 +26,6 @@ public class Customer {
         StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 //      비디오 종류별 대여료 계산
         for(Rental each : rentals){
-            double thisAmount = each.getCharge();
-            
 //          적립 포인트를 1포인트 증가
             frequentRenterPoints++;
 //          최신물을 이틀 이상 대여하면 보너스 포인트 지급
@@ -37,10 +35,10 @@ public class Customer {
             
 //          이번에 대여하는 비디오 정보와 대여료를 출력
             result.append("\t"+each.getMovie().getTitle());
-            result.append("\t"+String.valueOf(thisAmount)+"\n");
+            result.append("\t"+String.valueOf(each.getCharge())+"\n");
             
 //          현재까지 누적된 총 대여료
-            totalAmount += thisAmount;
+            totalAmount += each.getCharge();
         }//end of for-loop
         
 //      푸터행 추가
