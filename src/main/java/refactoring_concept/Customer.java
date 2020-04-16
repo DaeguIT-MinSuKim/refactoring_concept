775ebff46514e99099f134391788640d094f6907
teacher <net94.teacher@gmail.com>
2020-04-16 10:39:38 +0900

    extract_method and Replace Temp with Query

--- src/main/java/refactoring_concept/Customer.java
+++ src/main/java/refactoring_concept/Customer.java
@@ -20,30 +20,35 @@ public class Customer {
     }
 
     public String statement(){
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        
         StringBuilder result = new StringBuilder(getName() + " 고객님의 대여 기록\n");
 //      비디오 종류별 대여료 계산
         for(Rental each : rentals){
-//          적립 포인트를 1포인트 증가
-            frequentRenterPoints += each.getFrequentRenterPoints();
-            
 //          이번에 대여하는 비디오 정보와 대여료를 출력
             result.append("\t"+each.getMovie().getTitle());
             result.append("\t"+String.valueOf(each.getCharge())+"\n");
-            
-//          현재까지 누적된 총 대여료
-            totalAmount += each.getCharge();
         }//end of for-loop
         
 //      푸터행 추가
-        result.append("누적 대여료 : " + String.valueOf(totalAmount) + "\n");
-        result.append("적립 포인트 : " + String.valueOf(frequentRenterPoints));
+        result.append("누적 대여료 : " + String.valueOf(getTotalCharge()) + "\n");
+        result.append("적립 포인트 : " + String.valueOf(getTotalFrequentRenterPoints()));
         
         return result.toString();
     }//end of method statement
 
-	
+	private int getTotalFrequentRenterPoints() {
+		int result = 0;
+		for(Rental each : rentals){
+			result += each.getFrequentRenterPoints();
+		}
+		return result;
+	}
+
+	private int getTotalCharge() {
+		int result = 0;
+		for(Rental each : rentals){
+			result += each.getCharge();
+		}
+		return result;
+	}
 
 }
